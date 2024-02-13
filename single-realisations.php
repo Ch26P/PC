87637ba11d8f37f609bc9eed334b50730b2e3d6a
Pochon Christophe <chris26-78@hotmail.fr>
2024-02-13 01:38:42 +0100

    single-realisation

--- single-realisations.php
+++ single-realisations.php
@@ -7,7 +7,51 @@ while (have_posts()) :
 ?>
 	<main>
 		<div class="content">
-			<h1><?php the_title(); ?></h1>
+
+
+
+			<div class="content_rea_info">
+				<h1><?php the_title(); ?></h1>
+				<p> <?php echo (get_field('type_de_realisations')); ?> réalisé pour <?php echo (get_field('mission')); ?></p>
+				<h2><?php echo (get_field('nom_missionnaire')); ?></h2>
+
+
+				<?php /*
+				$list_champs = get_fields();
+				if (get_fields() !== false) {
+					foreach ($list_champs as $name => $value) {
+						if ($name === "CMS") {
+							if ($value === "oui") {
+								echo '<p>Ce site à était conçu avec </p>';
+							}
+						} elseif ($value === "pluging") {
+							echo '<p>Ce pluging à était conçu pour <p>';
+						}
+
+
+						if ($name === "choix_cms") {
+
+							if ($value === "WORDPRESS") {
+
+								echo ('<i class="fa-brands fa-wordpress"></i>');
+							} elseif ($value === "PRESTASHOP") {
+								//a finir
+							}
+						}
+					}
+				} */ ?>
+			</div>
+
+
+
+
+
+
+
+
+
+
+
 			<div class="content_supports">
 				<div class="computer">
 					<img class="supports computer" src="<?php echo get_template_directory_uri() . ' /assets/images/computer-libre.webp' ?> ">
@@ -61,37 +105,74 @@ while (have_posts()) :
 					?>
 				</div>
 			</div>
-			<div class="info_rea">
-				<?php
-				$list_champs = get_fields();
-				if (get_fields() !== false) : ?>
-					<?php foreach ($list_champs as $name => $value) : ?>
-						<?php if ($name === "CMS") : ?>
-							<?php if ($value === "oui") : ?>
-								<p>ok</p>
+
+			<div class="info_realisation">
+				<h3>Caracteristiques technique</h3>
+				<div class="info_realisation_techno">
+					<div class="info_realisation_techno_left">
+						<?php
+
+						if (get_field('choix_cms')/* !== false*/) {
+							foreach ($list_champs as $name => $value) {
+
+								if ($name === "choix_cms") {
+
+									if ($value === "WORDPRESS") {
+
+										echo ('<i class="fa-brands fa-wordpress"></i>');
+									} elseif ($value === "PRESTASHOP") {
+										//a finir
+									}
+								}
+								if ($name === "version") {
+									echo '<p>version:' . esc_html(get_field('version')) . '</p>';
+								}
+							}
+						} ?>
+					</div>
+					<div class="info_realisation_techno_right">
+						<?php
+
+						$technos = get_field('technologies_utilisees');
+						if ($technos) : ?>
+							<ul class="techno">
+								<?php foreach ($technos as $techno) {
+									if ($techno === "JS") {
+										echo	'<li><i class="fa-brands fa-js"></i></li>';
+									}
+									if ($techno === "HTML5") {
+										echo	'<li><i class="fa-brands fa-html5"></i></li>';
+									}
+									if ($techno === "CSS3") {
+										echo	'<li><i class="fa-brands fa-css3"></i></li>';
+									}
+									if ($techno === "SASS") {
+										echo	'<li><i class="fa-brands fa-sass"></i></li>';
+									}
+									if ($techno === "PHP") {
+										echo	'<li><i class="fa-brands fa-php"></i></li>';
+									}
+								} ?>
+							</ul>
+					</div>
+				</div>
+				<div>
+					<div class="bloc_description">
+						<div class="description desc_m">
+							<h3>Description de la mission</h3>
+							<p><?php echo wp_kses_post(get_field('description_mission')); ?></p>
+						</div>
+						<div class="description desc_r">
+							<h3>Description de la Réalisation</h3>
+							<p><?php echo wp_kses_post(get_field('description_realisation')); ?></p>
+						</div>
+					</div>
+				</div>
+			<?php endif; ?>
 
 
-
-
-							<?php endif;
-						endif;
-
-						if ($name === "CMS") : ?>
-							<?php if ($value === "oui") : ?>
-								<p>ok</p>
-
-
-
-
-				<?php endif;
-						endif;
-
-
-
-					endforeach;
-				endif; ?>
-				<i class="fa-brands fa-css3" style="color: #74C0FC;"></i>
 			</div>
+
 
 
 
